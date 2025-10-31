abcd32bec2127d32344ef071563a277fb2fd7b60
Thomas7899 <142595939+Thomas7899@users.noreply.github.com>
2025-10-31 19:12:54 +0100

    fix:users/route.ts auth

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,4 @@
-import { auth } from "@/app/api/auth/[...nextauth]/route";
+import { auth } from "@/lib/auth"
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { z } from "zod";
